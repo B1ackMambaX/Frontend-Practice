9733935494c7f32a915cce36f253dbc09031e59e
B1ackMambaX <b1ackmambax@yandex.ru>
2023-04-11 23:50:04 +0500

    added simple tabs

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -1,17 +1,59 @@
 const slider = tns({
-  container: '.carousel__inner',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  controls: false,
-  nav: false,
-  speed: 800
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    speed: 800
 });
 
 document.querySelector('.prev').addEventListener('click', () => {
-  slider.goTo('prev');
+    slider.goTo('prev');
 });
 document.querySelector('.next').addEventListener('click', () => {
-  slider.goTo('next');
+    slider.goTo('next');
 });
-      
+
+const tab = document.querySelector('.catalog__tabs');
+const tabs = document.querySelectorAll('.catalog__tab');
+const pages = document.querySelectorAll('.catalog__content');
+const linksToDescr = document.querySelectorAll('.catalog-item__link');
+const descr = document.querySelectorAll('.catalog-item__list');
+const linksToCards = document.querySelectorAll('.catalog-item__back');
+const cards = document.querySelectorAll('.catalog-item__content');
+
+function pagesSwitcher(tabsWrapper, curTab, activityClassTabs, pages, activityClassPages){
+    for(let i = 0; i < tabsWrapper.children.length; i++) {
+        if(tabsWrapper.children[i] == curTab){
+            tabsWrapper.children[i].classList.add(activityClassTabs);
+            pages[i].classList.add(activityClassPages);
+        }
+        else {
+            tabsWrapper.children[i].classList.remove(activityClassTabs);
+            pages[i].classList.remove(activityClassPages);
+        }
+    }
+}
+
+tabs.forEach((elem, key, arr) => {
+    elem.addEventListener('click',() => {
+	pagesSwitcher(tab, arr[key], 'catalog__tab_active', pages, 'catalog__content_active');
+    });
+});
+
+linksToDescr.forEach((elem, key) => {
+    elem.addEventListener('click', () => {
+        descr[key].classList.add('catalog-item__list_active');
+        cards[key].classList.remove('catalog-item__content_active');
+    });
+});
+
+linksToCards.forEach((elem, key) => {
+    elem.addEventListener('click', () => {
+        descr[key].classList.remove('catalog-item__list_active');
+        cards[key].classList.add('catalog-item__content_active');
+    });
+});
+
+	  
