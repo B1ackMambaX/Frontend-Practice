e48593d3b85740628127134dac4aa26dbe175f9e
B1ackMambaX <b1ackmambax@yandex.ru>
2023-05-11 15:15:20 +0500

    added mailer and animations

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -76,34 +76,73 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
     //Validation
+    function validateForm(form){
+        $(form).validate({
+            rules:{
+                name:"required",
+                phone:"required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+        
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                },
+                phone: "Пожалуйста, введите свой номер телефона"
+            }
+        });
+    }
     validateForm('#main-form');
     validateForm("#consultation form");
     validateForm('#order form');
+
     //Phone mask
     $('input[name=phone]').mask("+7 (999) 999-99-99");
-});
 
-function validateForm(form){
-    $(form).validate({
-        rules:{
-            name:"required",
-            phone:"required",
-            email: {
-                required: true,
-                email: true
-            }
-        },
-    
-        messages: {
-            name: "Пожалуйста, введите свое имя",
-            email: {
-              required: "Пожалуйста, введите свою почту",
-              email: "Неправильно введен адрес почты"
-            },
-            phone: "Пожалуйста, введите свой номер телефона"
+    //Mailer
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if(!$(this).validate()){
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Smooth scroll and pageup
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
         }
     });
-}
 
-	  
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    //Animations
+    new WOW().init();
+});	  
